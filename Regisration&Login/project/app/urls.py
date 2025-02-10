072fc6fc9368dc970a41a6a14e765444a93b0ecb
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-10 10:27:31 +0530

    m

--- Regisration&Login/project/app/urls.py
+++ Regisration&Login/project/app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
-    path('logout/',views.logout,name='logout'),
-    path('dashboard/',views.dashboard,name='dashboard'),
+    # path('logout/',views.logout,name='logout'),
+    # path('dashboard/',views.dashboard,name='dashboard'),
     
 ]
